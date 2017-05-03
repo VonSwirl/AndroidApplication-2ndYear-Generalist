6bd74d444bec411864e8ed4d4b8f574e8a72ed46
Rob <rib1356@hotmail.co.uk>
2017-05-03 01:42:48 +0100

    Small tweeks make for QOL Scroll to popup and stopped text going behind buttons

--- app/src/main/java/uk/ac/tees/com2060/kitkat/generalist/Popup.java
+++ app/src/main/java/uk/ac/tees/com2060/kitkat/generalist/Popup.java
@@ -1,12 +1,27 @@
 package uk.ac.tees.com2060.kitkat.generalist;
 
+import android.app.ListActivity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.LayoutRes;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.CheckedTextView;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Popup extends AppCompatActivity {
@@ -20,6 +35,7 @@ public class Popup extends AppCompatActivity {
         //TODO: When there are mutliple values in the list stop it going off the screen (DO THE SAME FOR EDIT AND ADD)!
         //TODO: Sort the padding so it is equal on both the x and y axis
 
+        //Display as a popup
         DisplayMetrics dm = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(dm);
 
@@ -28,9 +44,8 @@ public class Popup extends AppCompatActivity {
 
         getWindow().setLayout((int) (width * .85), (int) (height * .75)); //Sets the size of the popup window
 
-
         final TextView viewContents = (TextView) findViewById(R.id.view_contents);
-       // Button cnclBtn = (Button) findViewById(R.id.cancel_button);
+        // Button cnclBtn = (Button) findViewById(R.id.cancel_button);
 
         Intent intent = getIntent();//Create new getIntent
         final int position = intent.getIntExtra("position", 0); //Use it to pass the position from "ViewListActivity"
